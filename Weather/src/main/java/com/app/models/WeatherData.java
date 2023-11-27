a29b7883d4abbb6603bb3b4dd7c193d82eb6a1c7
vaishkiyarminal001 <vaishkiyarminal001@gmail.com>
2023-11-27 16:33:49 +0530

    Added

--- Weather/src/main/java/com/app/models/WeatherData.java
+++ Weather/src/main/java/com/app/models/WeatherData.java
@@ -16,7 +16,9 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
-
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Table(name = "weather_data")
 public class WeatherData {
     @Id
@@ -38,57 +40,6 @@ public class WeatherData {
     @Column(nullable = false)
     @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     private LocalDateTime timestamp = LocalDateTime.now();
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getCity() {
-		return city;
-	}
-
-	public void setCity(String city) {
-		this.city = city;
-	}
-
-	public double getTemperature() {
-		return temperature;
-	}
-
-	public void setTemperature(double temperature) {
-		this.temperature = temperature;
-	}
-
-	public double getHumidity() {
-		return humidity;
-	}
-
-	public void setHumidity(double humidity) {
-		this.humidity = humidity;
-	}
-
-	public double getWind() {
-		return wind;
-	}
-
-	public void setWind(double wind) {
-		this.wind = wind;
-	}
-
-	public LocalDateTime getTimestamp() {
-		return timestamp;
-	}
-
-	public void setTimestamp(LocalDateTime timestamp) {
-		this.timestamp = timestamp;
-	}
-    
-    
-    
 
        
 }
